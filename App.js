2261d423b0441deb86c05889ecb218be4b484b28
yairhdz24 <yairhdz107@gmail.com>
2025-02-27 04:12:22 -0600

    YA FUNCIONAAA LAVVVVVVVVV

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import HomeScreen from './screens/HomeScreen';
-import SplashScreen from './screens/SplashScreen';
-import LoginScreen from './screens/LoginScreen';
-import MainHomeScreen from './screens/MainHomeScreen';
-import TestScreen from './screens/TestScreen';
-import ResultsScreen from './screens/ResultsScreen';
-import ForumScreen from './screens/ForumScreen';
-import DirectoryScreen from './screens/DirectoryScreen';  
-import LocationScreen from './screens/LocationScreen';
-import ContactsScreen from './screens/ContactsScreen';
-import HistoryScreen from './screens/HistoryScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import HelpScreen from './screens/HelpScreen';
-import CalculatorScreen from './screens/CalculatorScreen';
+// import "react-native-gesture-handler"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import HomeScreen from "./HomeScreen"
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
@@ -27,25 +14,13 @@ export default function App() {
           initialRouteName="Welcome"
           screenOptions={{
             headerShown: false,
-            cardStyle: { backgroundColor: '#F5F7FA' },
+            cardStyle: { backgroundColor: "#FFF0F5" },
           }}
         >
           <Stack.Screen name="Welcome" component={HomeScreen} />
-          <Stack.Screen name="Splash" component={SplashScreen} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="MainHome" component={MainHomeScreen} />
-          <Stack.Screen name="Test" component={TestScreen} />
-          <Stack.Screen name="Results" component={ResultsScreen} />
-          <Stack.Screen name="Forum" component={ForumScreen} />
-          <Stack.Screen name="Directory" component={DirectoryScreen} />
-          <Stack.Screen name="Location" component={LocationScreen} />
-          <Stack.Screen name="Contacts" component={ContactsScreen} />
-          <Stack.Screen name="History" component={HistoryScreen} />
-          <Stack.Screen name="Settings" component={SettingsScreen} />
-          <Stack.Screen name="Help" component={HelpScreen} />
-          <Stack.Screen name="Calculator" component={CalculatorScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
-  );
+  )
 }
+
